8fdb3579347957e3c04ab094b298203b733f761f
FrankHsiao <lordarcher12345@gmail.com>
2023-04-17 23:10:30 +0800

    hello

---
+++ src/main/java/com/frank/review/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.frank.review.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@GetMapping(path = "/hello")
+	public String hello() {
+		
+		return "Hello springboot is runnig.... good !";
+	}
+	
+}
+
+
+
+
+
