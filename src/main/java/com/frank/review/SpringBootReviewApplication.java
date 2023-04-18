1e615cb8c9c24051aa6fa6cd3bc776b1b9f31c28
FrankHsiao <lordarcher12345@gmail.com>
2023-04-18 22:49:49 +0800

    jetty & starter

--- src/main/java/com/frank/review/SpringBootReviewApplication.java
+++ src/main/java/com/frank/review/SpringBootReviewApplication.java
@@ -2,12 +2,18 @@ package com.frank.review;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.frank.review.controller.HelloController;
 
 @SpringBootApplication
 public class SpringBootReviewApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootReviewApplication.class, args);
+        ConfigurableApplicationContext ctx = 
+        		SpringApplication.run(SpringBootReviewApplication.class, args);
+//        HelloController helloCon = ctx.getBean(HelloController.class);
+//        System.out.println("bean====>" + helloCon);
     }
 
 }
